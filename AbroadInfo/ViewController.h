63dd36d215369dc3fa173684a74295c813d445fb
prakash Maharjan <prakashmaharjan@prakashs-macbook-pro.local>
2015-10-25 15:24:27 +0545

    Initial Commit

---
+++ AbroadInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AbroadInfo
+//
+//  Created by prakash Maharjan on 10/25/15.
+//  Copyright © 2015 Game Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AbroadInfo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AbroadInfo
+//
+//  Created by prakash Maharjan on 10/25/15.
+//  Copyright © 2015 Game Studios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
